c16ebf9e43ec5a22ba78558a3fcc57b1d9465203
jyothivaranasi <74580244+jyothivaranasi@users.noreply.github.com>
2020-11-17 09:38:36 +0530

    first upload

---
+++ PositivrNegative.java
@@ -0,0 +1,11 @@
+package com.labbook2;
+
+public class PositivrNegative {
+public static void main(String[] args) {
+int p=Integer.parseInt(args[0]);
+if(p>0) 
+	System.out.println("Positive");
+else
+	System.out.println("Negavtie");
+}
+}
